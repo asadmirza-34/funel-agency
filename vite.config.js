5498ed96cbbca4b1d5be8a690ee8f65213d0bc99
asadmirza-34 <asadmirza2034@gmail.com>
2026-05-10 15:34:15 +0500

    Fix: base path for both Vercel and GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/funel-agency/',
+  base: process.env.VERCEL ? '/' : '/funel-agency/',
   plugins: [react()],
 })
